83740ccd3c8c25f2f19291a755fe682f59f00f85
Kim Drezdon <kdrezdon@gmail.com>
2019-10-19 14:23:15 -0500

    Apply provided css and check connection to API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    courses: []
+  };
+  
+  componentDidMount() {
+    fetch('http://localhost:5000/api/courses')
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ courses: responseData });
+        console.log(this.state.courses);
+      })
+      .catch(error => {
+        console.log('Error fetching data', error);
+      });
+  };
+  
+  render () {
+    const courseList = this.state.courses.map(course =>
+        <li key={course.id}>{course.title}</li>
+    )
+
+    return (
+      <div className="App">
+        <ul>
+          {courseList}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
