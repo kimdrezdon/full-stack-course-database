e8186e5d649e0dfe5aa245c5aafdcccd01f1da5d
Kim Drezdon <kdrezdon@gmail.com>
2019-10-20 10:28:42 -0500

    Add comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,20 @@
+// Entry point into the application which renders the main App component
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// CSS files
 import './styles/global.css';
 import './index.css';
+
+// App component
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
