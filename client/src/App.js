16aad08bb0847900feb9e1e283ead7334e224da1
Kim Drezdon <kdrezdon@gmail.com>
2019-10-20 13:54:47 -0500

    Add BrowserRouter and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-//import Courses from "./components/Courses";
-// import CourseDetail from "./components/CourseDetail";
-// import UserSignIn from "./components/UserSignIn";
-// import CreateCourse from "./components/CreateCourse";
-// import UpdateCourse from "./components/UpdateCourse";
+import Courses from "./components/Courses";
+import CourseDetail from "./components/CourseDetail";
+import UserSignIn from "./components/UserSignIn";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from "./components/UpdateCourse";
 import UserSignUp from "./components/UserSignUp";
 import Header from "./components/Header";
+import UserSignOut from './components/UserSignOut';
 
-const App = () => {
-  return (  
+const App = () => (
+  <BrowserRouter>
     <div>
       <Header />
       <hr></hr>
-      <UserSignUp />
+      <Route exact path="/" component={Courses} />
+      <Route path="/courses/create" component={CreateCourse} />
+      <Route path="/courses/:id/update" component={UpdateCourse} />
+      <Route path="/courses/:id" component={CourseDetail} />
+      <Route path="/signin" component={UserSignIn} />
+      <Route path="/signup" component={UserSignUp} />
+      <Route path="/signout" component={UserSignOut} />
     </div>
-  );
-}
- 
+  </BrowserRouter>
+  
+)
+
 export default App;
--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const UserSignOut = () => {
-    return (  );
+    return ( 
+        <div>
+            <p>You're signed out!</p>
+        </div>
+    );
 }
  
 export default UserSignOut;
