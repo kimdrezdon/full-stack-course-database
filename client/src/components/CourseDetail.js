8fb7ea57c8e33b8bb32d5bf950daa8dce00de9c5
Kim Drezdon <kdrezdon@gmail.com>
2019-10-28 16:54:06 -0500

    Populate course detail page with data from rest API

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,37 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+const ReactMarkdown = require('react-markdown');
 
 class CourseDetail extends Component {
     state = {
-        courses: []
+        course: {},
+        courseOwner: {}
     };
     
     componentDidMount() {
-        this.loadCourses();
-    };
-
-    loadCourses = () => {
-        fetch('http://localhost:5000/api/courses')
+        const url = 'http://localhost:5000/api/courses/' + this.props.match.params.id;
+        fetch(url)
             .then(response => response.json())
             .then(responseData => {
-                this.setState({ courses: responseData });
+                this.setState({ course: responseData, courseOwner: responseData.User });
             })
             .catch(error => {
                 console.log('Error fetching data', error);
             });
+    };
+
+    deleteCourse = (courseId) => {
+        const url = 'http://localhost:5000/api/courses/' + courseId;
+        const options = { method: 'DELETE' }
+        fetch(url, options);
     }
 
-    render() { 
-        const courseId = this.props.match.params.id;
-        
+    render() {
+        const { course, courseOwner } = this.state;
+
         return (
             <div>
                 <div className="actions--bar">
                     <div className="bounds">
                         <div className="grid-100">
                             <span>
-                                <Link to={`/courses/${courseId}/update`} className="button">Update Course</Link>
-                                <Link to="/courses" className="button">Delete Course</Link>
+                                <Link to={`/courses/${this.props.match.params.id}/update`} className="button">Update Course</Link>
+                                <Link to="/courses" onClick={() => {this.deleteCourse(this.props.match.params.id)}} className="button">Delete Course</Link>
                             </span>
                             <Link to="/courses/" className="button button-secondary">Return to List</Link>
                         </div>
@@ -41,11 +46,11 @@ class CourseDetail extends Component {
                     <div className="grid-66">
                         <div className="course--header">
                             <h4 className="course--label">Course</h4>
-                            <h3 className="course--title">Course Title</h3>
-                            {/* <p>By {selectedCourse.userId}</p> */}
+                            <h3 className="course--title">{course.title}</h3>
+                            <p>By {courseOwner.firstName} {courseOwner.lastName}</p>
                         </div>
                         <div className="course--description">
-                            {/* <p>{selectedCourse.description}</p> */}
+                            <ReactMarkdown source={course.description} />
                         </div>
                     </div>
                     <div className="grid-25 grid-right">
@@ -53,13 +58,11 @@ class CourseDetail extends Component {
                             <ul className="course--stats--list">
                                 <li className="course--stats--list--item">
                                     <h4>Estimated Time</h4>
-                                    {/* <h3>{selectedCourse.estimatedTime}</h3> */}
+                                    <h3>{course.estimatedTime}</h3>
                                 </li>
                                 <li className="course--stats--list--item">
                                     <h4>Materials Needed</h4>
-                                    <ul>
-                                        {/* <li>{selectedCourse.materialsNeeded}</li> */}
-                                    </ul>
+                                    <ReactMarkdown source={course.materialsNeeded} />
                                 </li>
                             </ul>
                         </div>
