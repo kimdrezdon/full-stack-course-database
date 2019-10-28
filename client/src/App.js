fb590c42bbed6a14a26a4b28757fb9983e64b63c
Kim Drezdon <kdrezdon@gmail.com>
2019-10-28 17:22:23 -0500

    Added NotFound component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import Courses from "./components/Courses";
 import CourseDetail from "./components/CourseDetail";
@@ -9,20 +9,24 @@ import UpdateCourse from "./components/UpdateCourse";
 import UserSignUp from "./components/UserSignUp";
 import Header from "./components/Header";
 import UserSignOut from './components/UserSignOut';
+import NotFound from './components/NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Header />
       <hr></hr>
-      <Route exact path="/" render={() => <Redirect to ="/courses" />} />
-      <Route exact path="/courses" component={Courses} />
-      <Route path="/courses/create" component={CreateCourse} />
-      <Route path="/courses/:id/update" component={UpdateCourse} />
-      <Route exact path="/courses/:id" component={CourseDetail} />
-      <Route path="/signin" component={UserSignIn} />
-      <Route path="/signup" component={UserSignUp} />
-      <Route path="/signout" component={UserSignOut} />
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to ="/courses" />} />
+        <Route exact path="/courses" component={Courses} />
+        <Route path="/courses/create" component={CreateCourse} />
+        <Route path="/courses/:id/update" component={UpdateCourse} />
+        <Route exact path="/courses/:id" component={CourseDetail} />
+        <Route path="/signin" component={UserSignIn} />
+        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signout" component={UserSignOut} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
   
