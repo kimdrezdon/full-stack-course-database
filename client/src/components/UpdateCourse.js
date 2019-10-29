092687a0603c9ee259e0cb326affbd319d80d276
Kim Drezdon <kdrezdon@gmail.com>
2019-10-28 19:07:04 -0500

    Turn update form into controlled component

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 
 class UpdateCourse extends Component {
-    state = {  }
+    state = {
+        courseOwner: {},
+        courseTitle: '',
+        courseDescription: '',
+        courseEstimatedTime: '',
+        courseMaterialsNeeded: ''
+    }
+
+    componentDidMount() {
+        const url = 'http://localhost:5000/api/courses/' + this.props.match.params.id;
+        fetch(url)
+            .then(response => response.json())
+            .then(responseData => {
+                this.setState({
+                    courseOwner: responseData.User,
+                    courseTitle: responseData.title,
+                    courseDescription: responseData.description,
+                    courseEstimatedTime: (responseData.estimatedTime ? responseData.estimatedTime : ''),
+                    courseMaterialsNeeded: (responseData.materialsNeeded ? responseData.materialsNeeded : '')
+                });
+            })
+            .catch(error => {
+                console.log('Error fetching data', error);
+            });
+    }
+
+    updateCourse = (courseId) => {
+        const url = 'http://localhost:5000/api/courses/' + courseId;
+        const options = { method: 'PUT' }
+        fetch(url, options);
+        this.props.history.push(`/courses/${courseId}`);
+    }
 
     handleCancel = e => {
         e.preventDefault();
@@ -9,7 +40,23 @@ class UpdateCourse extends Component {
         this.props.history.push(`/courses/${courseId}`);
     }
 
+    handleChange = e => {
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({
+            [name] : value
+        })
+    }
+
     render() { 
+        const {
+            courseOwner,
+            courseTitle,
+            courseDescription,
+            courseEstimatedTime,
+            courseMaterialsNeeded
+        } = this.state;
+
         return ( 
             <div className="bounds course--detail">
                 <h1>Update Course</h1>
@@ -19,14 +66,26 @@ class UpdateCourse extends Component {
                             <div className="course--header">
                                 <h4 className="course--label">Course</h4>
                                 <div>
-                                    <input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
-                                    value="Build a Basic Bookcase"></input>
+                                    <input 
+                                        id="title" 
+                                        name="courseTitle" 
+                                        type="text" 
+                                        className="input-title course--title--input" 
+                                        placeholder="Course title..."
+                                        value={courseTitle} 
+                                        onChange={this.handleChange} />
                                 </div>
-                                <p>By Joe Smith</p>
+                                <p>By {courseOwner.firstName} {courseOwner.lastName}</p>
                             </div>
                             <div className="course--description">
                                 <div>
-                                    <textarea id="description" name="description" className="" placeholder="Course description...">High-end furniture projects are great to dream about. But unless you have a well-equipped shop and some serious woodworking experience to draw on, it can be difficult to turn the dream into a reality.</textarea>
+                                    <textarea 
+                                        id="description" 
+                                        name="courseDescription" 
+                                        className="" 
+                                        placeholder="Course description..." 
+                                        value={courseDescription}
+                                        onChange={this.handleChange} />
                                 </div>
                             </div>
                         </div>
@@ -36,21 +95,33 @@ class UpdateCourse extends Component {
                                     <li className="course--stats--list--item">
                                         <h4>Estimated Time</h4>
                                         <div>
-                                            <input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
-                                            placeholder="Hours" value="14 hours"></input>
+                                            <input 
+                                                id="estimatedTime" 
+                                                name="courseEstimatedTime" 
+                                                type="text" 
+                                                className="course--time--input"
+                                                placeholder="Hours" 
+                                                value={courseEstimatedTime}
+                                                onChange={this.handleChange} />
                                         </div>
                                     </li>
                                     <li className="course--stats--list--item">
                                         <h4>Materials Needed</h4>
                                         <div>
-                                            <textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials...">1/2 x 3/4 inch parting strip</textarea>
+                                            <textarea 
+                                                id="materialsNeeded" 
+                                                name="courseMaterialsNeeded" 
+                                                className="" 
+                                                placeholder="List materials..." 
+                                                value={courseMaterialsNeeded}
+                                                onChange={this.handleChange} />
                                         </div>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                         <div className="grid-100 pad-bottom">
-                            <button className="button" type="submit">Update Course</button>
+                            <button onClick={() => {this.updateCourse(this.props.match.params.id)}} className="button" type="submit">Update Course</button>
                             <button className="button button-secondary" onClick={this.handleCancel}>Cancel</button>
                         </div>
                     </form>
