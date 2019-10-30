d1339bdeb6201885018d42019aa44c86c64297af
Kim Drezdon <kdrezdon@gmail.com>
2019-10-30 11:30:37 -0500

    Add components and withContext function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
+//import the withContext function from Context
+import withContext from './Context';
+
 import Courses from "./components/Courses";
 import CourseDetail from "./components/CourseDetail";
 import UserSignIn from "./components/UserSignIn";
@@ -10,6 +13,10 @@ import UserSignUp from "./components/UserSignUp";
 import Header from "./components/Header";
 import UserSignOut from './components/UserSignOut';
 import NotFound from './components/NotFound';
+import UnhandledError from './components/UnhandledError';
+
+//connect the UserSignUp component to context
+const UserSignUpWithContext = withContext(UserSignUp);
 
 const App = () => (
   <BrowserRouter>
@@ -23,8 +30,10 @@ const App = () => (
         <Route path="/courses/:id/update" component={UpdateCourse} />
         <Route exact path="/courses/:id" component={CourseDetail} />
         <Route path="/signin" component={UserSignIn} />
-        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOut} />
+        <Route path="/notfound" component={NotFound} />
+        <Route path="/error" component={UnhandledError} />
         <Route component={NotFound} />
       </Switch>
     </div>
