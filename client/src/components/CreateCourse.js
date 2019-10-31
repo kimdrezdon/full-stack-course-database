0f37cb90288fbf9f80042253263a4f8c1a5badf7
Kim Drezdon <kdrezdon@gmail.com>
2019-10-31 12:54:28 -0500

    Turn form into a controlled component with context

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,40 +1,85 @@
 import React, { Component } from 'react';
+import ErrorsDisplay from './ErrorsDisplay';
 
 class CreateCourse extends Component {
-    state = {  }
+    state = { 
+        title: '',
+        description: '',
+        estimatedTime: '',
+        materialsNeeded: '',
+        errors: []
+    }
 
     handleCancel = e => {
         e.preventDefault();
         this.props.history.push('/courses');
     }
 
+    handleChange = e => {
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({
+            [name] : value
+        })
+    }
+    
+    handleSubmit = e => {
+        e.preventDefault();
+        if (this.props.context.authenticatedUser) {
+            const emailAddress = this.props.context.authenticatedUser.emailAddress;
+            const password = this.props.context.authenticatedUser.password;
+            console.log(emailAddress, password);
+            const courseData = {
+                title: this.state.title,
+                description: this.state.description,
+                estimatedTime: this.state.estimatedTime,
+                materialsNeeded: this.state.materialsNeeded
+            };
+            this.props.context.actions.callApi('/courses', 'POST', courseData, true, {emailAddress, password})
+        } else {
+            this.props.history.push('/signin');
+        }
+    }
+
     render() { 
+        const {
+            title,
+            description,
+            estimatedTime,
+            materialsNeeded,
+            errors
+        } = this.state;
+
         return ( 
             <div className="bounds course--detail">
                 <h1>Create Course</h1>
                 <div>
-                    <div>
-                        <h2 className="validation--errors--label">Validation errors</h2>
-                        <div className="validation-errors">
-                            <ul>
-                                <li>Please provide a value for "Title"</li>
-                                <li>Please provide a value for "Description"</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <form>
+                    <ErrorsDisplay errors={errors} />
+                    <form onSubmit={this.handleSubmit}>
                         <div className="grid-66">
                             <div className="course--header">
                                 <h4 className="course--label">Course</h4>
                                 <div>
-                                    <input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
-                                    value=""></input>
+                                    <input 
+                                        id="title" 
+                                        name="title" 
+                                        type="text" 
+                                        className="input-title course--title--input" 
+                                        placeholder="Course title..."
+                                        value={title}
+                                        onChange={this.handleChange} />
                                 </div>
                                 <p>By Joe Smith</p>
                             </div>
                             <div className="course--description">
                                 <div>
-                                    <textarea id="description" name="description" className="" placeholder="Course description..."></textarea>
+                                    <textarea 
+                                        id="description" 
+                                        name="description" 
+                                        className="" 
+                                        placeholder="Course description..."
+                                        value={description}
+                                        onChange={this.handleChange} />
                                 </div>
                             </div>
                         </div>
@@ -44,14 +89,26 @@ class CreateCourse extends Component {
                                     <li className="course--stats--list--item">
                                         <h4>Estimated Time</h4>
                                         <div>
-                                            <input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
-                                            placeholder="Hours" value=""></input>
+                                            <input 
+                                                id="estimatedTime" 
+                                                name="estimatedTime" 
+                                                type="text" 
+                                                className="course--time--input"
+                                                placeholder="Hours" 
+                                                value={estimatedTime}
+                                                onChange={this.handleChange} />
                                         </div>
                                     </li>
                                     <li className="course--stats--list--item">
                                         <h4>Materials Needed</h4>
                                         <div>
-                                            <textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..."></textarea>
+                                            <textarea 
+                                                id="materialsNeeded" 
+                                                name="materialsNeeded" 
+                                                className="" 
+                                                placeholder="List materials..."
+                                                value={materialsNeeded}
+                                                onChange={this.handleChange} />
                                         </div>
                                     </li>
                                 </ul>
