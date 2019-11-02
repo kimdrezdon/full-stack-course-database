95c2409aa16a3dbee0cec2f4f0187a6ea9022339
Kim Drezdon <kdrezdon@gmail.com>
2019-11-02 13:08:57 -0500

    Change Header to stateless functional component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Header extends Component {
-    render() {
-        const authenticatedUser = this.props.context.authenticatedUser;
+const Header = (props) => {
+    const authenticatedUser = props.context.authenticatedUser;
         
-        return ( 
-            <div className="header">
-                <div className="bounds">
-                    <h1 className="header--logo">Courses</h1>
-                    <nav>
-                        {authenticatedUser ? (
-                            <React.Fragment>
-                                <span>Welcome, {authenticatedUser.firstName}!</span>
-                                <Link to="/signout" className="signout">Sign Out</Link>
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>
-                                <Link to="/signup" className="signup">Sign Up</Link>
-                                <Link to="/signin" className="signin">Sign In</Link>
-                            </React.Fragment>
-                        )}
-                    </nav>
-                </div>
+    return ( 
+        <div className="header">
+            <div className="bounds">
+                <h1 className="header--logo">Courses</h1>
+                <nav>
+                    {authenticatedUser ? (
+                        <React.Fragment>
+                            <span>Welcome, {authenticatedUser.firstName}!</span>
+                            <Link to="/signout" className="signout">Sign Out</Link>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Link to="/signup" className="signup">Sign Up</Link>
+                            <Link to="/signin" className="signin">Sign In</Link>
+                        </React.Fragment>
+                    )}
+                </nav>
             </div>
-        );
-        }
+        </div>
+    );
 }
  
 export default Header;
