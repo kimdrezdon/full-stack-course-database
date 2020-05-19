608b412176c5aaaa43e4d2e1db72365d7dfd8c4c
Kim Drezdon <kdrezdon@gmail.com>
2020-05-19 10:23:17 -0500

    Clean up react files and add axios

--- client/src/index.js
+++ client/src/index.js
@@ -13,16 +13,9 @@ import { Provider } from './Context';
 // App component
 import App from './App';
 
-import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>, 
-    document.getElementById('root')
+	<Provider>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
